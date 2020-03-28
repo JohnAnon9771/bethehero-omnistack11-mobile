b58d00f224b249c6b2c4fe57d6efb10f720af47c
JohnAnon9771 <njoao97710@gmail.com>
2020-03-28 18:48:14 -0300

    finish syling and functions

--- src/screens/Details/index.js
+++ src/screens/Details/index.js
@@ -1,5 +1,74 @@
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { Feather } from '@expo/vector-icons';
+import { Image, TouchableOpacity, Linking } from 'react-native';
 
-export default function Details(){
-  return ()
+import * as MailComposer from 'expo-mail-composer';
+
+import {
+  Container,
+  Header,
+  Incident,
+  IncidentProperty,
+  IncidentValue,
+  Action,
+  Actions,
+  ActionText,
+  ContactBox,
+  HeroDescription,
+  HeroTitle,
+} from './styles';
+
+import logo from '../../assets/logo.png';
+
+export default function Details() {
+  const navigation = useNavigation();
+  const message =
+    'Olá APAD, estou entrando em contado pois gostaria de ajudar no caso "Caso1" com o valor de R$120,00';
+  function navigateToIncidents() {
+    navigation.goBack();
+  }
+
+  function sendMail() {
+    MailComposer.composeAsync({
+      subject: 'Herói do caso: caso1',
+      recipients: ['nicolesousa1306@gmail.com'],
+      body: message,
+    });
+  }
+
+  function sendWhatsapp() {
+    Linking.openURL(`whatsapp://send?phone=558881687878&text=${message}`);
+  }
+  return (
+    <Container>
+      <Header>
+        <Image source={logo} />
+        <TouchableOpacity onPress={navigateToIncidents}>
+          <Feather name="arrow-left" size={28} color="#e02041" />
+        </TouchableOpacity>
+      </Header>
+      <Incident>
+        <IncidentProperty style={{ marginTop: 0 }}>ONG:</IncidentProperty>
+        <IncidentValue>APAD</IncidentValue>
+        <IncidentProperty>CASO:</IncidentProperty>
+        <IncidentValue>teste</IncidentValue>
+        <IncidentProperty>VALOR:</IncidentProperty>
+        <IncidentValue>R$120,00</IncidentValue>
+      </Incident>
+      <ContactBox>
+        <HeroTitle>Salve o dia!</HeroTitle>
+        <HeroTitle>Seja o herói desse caso.</HeroTitle>
+        <HeroDescription>Entre em contato:</HeroDescription>
+        <Actions>
+          <Action onPress={sendWhatsapp}>
+            <ActionText>WhatsApp</ActionText>
+          </Action>
+          <Action onPress={sendMail}>
+            <ActionText>E-mail</ActionText>
+          </Action>
+        </Actions>
+      </ContactBox>
+    </Container>
+  );
 }
--- src/screens/Incidents/index.js
+++ src/screens/Incidents/index.js
@@ -1,5 +1,60 @@
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { Feather } from '@expo/vector-icons';
+import { Image } from 'react-native';
 
-export default function Incidents(){
-  return ()
+import {
+  Container,
+  Header,
+  HeaderText,
+  HeaderTextBold,
+  Title,
+  Description,
+  IncidentList,
+  Incident,
+  IncidentProperty,
+  IncidentValue,
+  DetailsButton,
+  DetailsButtonText,
+} from './styles';
+
+import logo from '../../assets/logo.png';
+
+export default function Incidents() {
+  const navigation = useNavigation();
+  function navigateToDetails() {
+    navigation.navigate('details');
+  }
+  return (
+    <Container>
+      <Header>
+        <Image source={logo} />
+        <HeaderText>
+          Total de <HeaderTextBold>0 casos</HeaderTextBold>.
+        </HeaderText>
+      </Header>
+      <Title>Bem-vindo!</Title>
+      <Description>Escolha um dos casos abaixos e salve o dia.</Description>
+      <IncidentList
+        data={[1, 2, 3]}
+        keyExtractor={(item, index) => `list-item${index}`}
+        showsVerticalScrollIndicator={false}
+        renderItem={() => (
+          <Incident>
+            <IncidentProperty>ONG:</IncidentProperty>
+            <IncidentValue>APAD</IncidentValue>
+            <IncidentProperty>CASO:</IncidentProperty>
+            <IncidentValue>teste</IncidentValue>
+            <IncidentProperty>VALOR:</IncidentProperty>
+            <IncidentValue>R$120,00</IncidentValue>
+
+            <DetailsButton onPress={navigateToDetails}>
+              <DetailsButtonText>Ver mais detalhes</DetailsButtonText>
+              <Feather name="arrow-right" size={16} color="#e02041" />
+            </DetailsButton>
+          </Incident>
+        )}
+      />
+    </Container>
+  );
 }
